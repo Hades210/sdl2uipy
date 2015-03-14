85a61fbef743b7b90dd92d7992cc6d7fa1be5d94
TriD <hovyakov@gmail.com>
2015-03-14 02:00:47 +0200

    getEvent returns nullptr if there was no events in queue

--- src/events.cpp
+++ src/events.cpp
@@ -2,6 +2,7 @@
 
 SDL_Event* getEvent() {
     SDL_Event* event = new SDL_Event();
-    SDL_PollEvent(event);
-    return event;
+    if (SDL_PollEvent(event))
+        return event;
+    return nullptr;
 }
